dfb8d72e8a884aba2f346062bf4331f2cb24ba57
Margaret Nottingham <peg@themnigroup.com>
2021-03-22 10:49:53 -0400

    hide stepper header. functionality tweaks.

--- website/src/app/pages/resources/survey/survey.component.ts
+++ website/src/app/pages/resources/survey/survey.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { SurveyService } from 'src/app/services/survey.service';
 @Component({
-  selector: 'app-survey',
-  templateUrl: './survey.component.html',
-  styleUrls: ['./survey.component.scss']
+	selector: 'app-survey',
+	templateUrl: './survey.component.html',
+	styleUrls: ['./survey.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class SurveyComponent implements OnInit {
     public defaults:any = this.surveyService.defaults;
     public values: any = this.defaults;
-    public formGroup: FormGroup; 
+    public formGroup: FormGroup;
 	constructor(
         private surveyService: SurveyService,
         private route: ActivatedRoute,
-	) { 
+	) {
         this.route.queryParams
             .subscribe(params => {
                 Object.keys(params).forEach((name:any) => {
                     let value:string = params[name];
                 });
             });
-            
     }
-
 	ngOnInit(): void {
         this.formGroup = new FormGroup({
             intent:	new FormControl(this.defaults.intent, Validators.required),
