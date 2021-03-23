90a01fc1aeb5f25f660364e1450cf5f17f0cf955
Margaret Nottingham <peg@themnigroup.com>
2021-03-23 12:54:03 -0400

    cleanup markdown additions

--- website/src/app/pages/resources/resources.component.ts
+++ website/src/app/pages/resources/resources.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SurveyService } from 'src/app/services/survey.service';
@@ -12,17 +11,11 @@ export class ResourcesComponent implements OnInit {
     public defaults:any = this.surveyService.defaults;
     public surveyInput: any = {};
     public readme: any;
-
-    private resourceMap: any = {
-        learn: {},
-        build:  {},
-    }
     constructor(
-        private httpClient: HttpClient,
         private surveyService: SurveyService,
 		private activatedRoute: ActivatedRoute,
         private router: Router
-	) { 
+	) {
         this.router.routeReuseStrategy.shouldReuseRoute = () => false;
         this.activatedRoute.queryParams
             .subscribe(params => {
@@ -34,18 +27,12 @@ export class ResourcesComponent implements OnInit {
                     let emptyValues:boolean = false;
                     Object.values(this.surveyInput).forEach((value:any) => {
                         if (value.length == 0)  emptyValues = true;
-                    })  
-                    if (emptyValues) this.showSurvey = true; 
+                    })
+                    if (emptyValues) this.showSurvey = true;
                     else this.showSurvey = false;
                 }
-                let permalink = "https://raw.githubusercontent.com/MLBiasgov/MLBias/8f2da741aefab7fedd979e2be4bb8faef8645971/README.md";
-                this.httpClient.get(permalink, { responseType: 'text'})
-                    .subscribe(
-                        readme => { this.readme = readme},
-                        error => { console.error(error)}    
-                    )
             });
-            
+
     }
     ngOnInit(): void {}
 }
