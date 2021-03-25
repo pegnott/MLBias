13453022a416b4bc1d5a6e0dc2ed41d34329cb5a
Margaret Nottingham <peg@themnigroup.com>
2021-03-25 16:32:52 -0400

    footer mobie order. home page button mobile text

--- website/src/app/pages/home/home.component.ts
+++ website/src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition,
+  } from '@angular/animations';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
+	animations: [
+		trigger('toggleClick',[
+			state('true', style({
+				backgroundColor: "green"
+			})),
+			state('false', style({
+				backgroundColor: "red"
+			})),
+			transition('true => false', animate('1000ms linear')),
+			transition('false => true', animate('1000ms linear'))
+		])
+	]
+
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	isGreen: string = 'true';
+ 	constructor() { }
+	ngOnInit(): void {	}
+	toggleIsCorrect() {
+		this.isGreen = this.isGreen === 'true' ? 'false' : 'true'; // change in data-bound value
+	}
 }
