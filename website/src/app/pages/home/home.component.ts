a8a8cff54b4dc7da9e8358210cd1d99646b68b2d
pegnott <peg.nottingham@gmail.com>
2021-03-26 08:09:18 -0400

    hide animation test

--- website/src/app/pages/home/home.component.ts
+++ website/src/app/pages/home/home.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {
-	trigger,
-	state,
-	style,
-	animate,
-	transition,
-  } from '@angular/animations';
+// import {
+// 	trigger,
+// 	state,
+// 	style,
+// 	animate,
+// 	transition,
+//   } from '@angular/animations';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
-	animations: [
-		trigger('toggleClick',[
-			state('true', style({
-				backgroundColor: "green"
-			})),
-			state('false', style({
-				backgroundColor: "red"
-			})),
-			transition('true => false', animate('1000ms linear')),
-			transition('false => true', animate('1000ms linear'))
-		])
-	]
+	// animations: [
+	// 	trigger('toggleClick',[
+	// 		state('true', style({
+	// 			backgroundColor: "green"
+	// 		})),
+	// 		state('false', style({
+	// 			backgroundColor: "red"
+	// 		})),
+	// 		transition('true => false', animate('1000ms linear')),
+	// 		transition('false => true', animate('1000ms linear'))
+	// 	])
+	// ]
 
 })
 export class HomeComponent implements OnInit {
